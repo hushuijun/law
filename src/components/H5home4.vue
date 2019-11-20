931dda642480a82a04c76f933ca1be072e64eed5
malihua <13683341906@163.com>
2019-11-20 15:25:42 +0800

    京师法律官网

--- src/components/H5home4.vue
+++ src/components/H5home4.vue
@@ -4,7 +4,12 @@
             <h3>核心服务</h3>
             <span class="line"></span>
         </div>
-        
+        <div>
+            <div class="BG4_BG">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -16,5 +21,10 @@ export default {
 }
 </script>
 <style scoped>
-    @import'../assets/css/home.css';
+@import'../assets/css/home.css';
+.BG4_BG div{
+    width:132px;
+    height:132px;
+    background: url('../assets/juxing.png') no-repeat center;
+}
 </style>
